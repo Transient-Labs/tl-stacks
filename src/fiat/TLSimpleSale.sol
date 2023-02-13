9cdcdb2d49514ed793f9c69e42da85934b845fd7
koloz <zach.kolodny@gmail.com>
2023-02-13 14:45:31 -0500

    starting crossmint sale minting contract

---
+++ src/fiat/TLSimpleSale.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.17;
+
+import {OwnableAccessControlUpgradeable} from "tl-sol-tools/upgradeable/access/OwnableAccessControlUpgradeable.sol";
+
+/*
+    ____        _ __    __   ____  _ ________                     __ 
+   / __ )__  __(_) /___/ /  / __ \(_) __/ __/__  ________  ____  / /_
+  / __  / / / / / / __  /  / / / / / /_/ /_/ _ \/ ___/ _ \/ __ \/ __/
+ / /_/ / /_/ / / / /_/ /  / /_/ / / __/ __/  __/ /  /  __/ / / / /__ 
+/_____/\__,_/_/_/\__,_/  /_____/_/_/ /_/  \___/_/   \___/_/ /_/\__(_)*/
+
+
+/// @title TLSimpleSale.sol
+/// @notice Transient Labs Contract for a Simple Sale
+/// @author transientlabs.xyz
+contract TLSimpleSale is OwnableAccessControlUpgradeable {}
